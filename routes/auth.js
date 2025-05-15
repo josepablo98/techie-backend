a473a33b2101e0ebba0c32103e8c04429bf8a3a0
Jose Pablo <josruiper5@alum.us.es>
2025-05-15 02:58:45 +0200

    refactorizaciones

--- routes/auth.js
+++ routes/auth.js
@@ -4,28 +4,14 @@ const { validatejwt } = require('../middlewares/validatejwt');
 
 const router = Router();
 
-router.post(
-  '/register',
-  createUser
-);
-
-router.post(
-  '/login',
-  loginUser
-);
-
+router.post('/register', createUser);
+router.post('/login', loginUser);
 router.get('/renew', validatejwt, revalidateToken);
-
 router.post('/request-password-reset', requestPasswordReset);
-
 router.post('/request-verified-email', requestVerifiedEmail);
-
 router.post('/logout', logoutUser);
-
 router.put('/reset-password', resetPassword);
-
 router.put('/verify-email', verifyEmail);
-
 router.delete('/delete-account', deleteUser);
 
 module.exports = router;
